022f8ec395fef684fb39155ba404ad834909d247
Comdev2k23 <comdev084@gmail.com>
2025-06-09 00:04:52 +0800

    updating

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // Parses incoming JSON requests
 
+app.get('/', (req, res)=> {
+    res.send("Welcome to the MSS-express-backend")
+})
+
 //  Routes
 app.use('/api/schedules', scheduleRoutes);
 app.use('/api/auth', authRoutes)  
