1deb2b7f073f4ee4e72bb4516dfc184cec8261c9
Comdev2k23 <comdev084@gmail.com>
2025-06-21 22:47:38 +0800

    updating search route 2

--- routes/user.route.js
+++ routes/user.route.js
@@ -10,6 +10,6 @@ userRoutes.get('/', protect, authorizeRoles('admin'), getAllUsers)
 userRoutes.get('/:id', protect, authorizeRoles('admin'), getUser)
 userRoutes.delete('/delete-user/:id', protect, authorizeRoles('admin'), deleteUser)
 userRoutes.patch('/update-user/:id', protect, validateUpdateRoleOnly, validateRequest, authorizeRoles('admin'), updateUser )
-userRoutes.get('/search', protect, authorizeRoles('admin'), searchUsers)
+userRoutes.get('/admin/search', protect, authorizeRoles('admin'), searchUsers)
 
 export default userRoutes
